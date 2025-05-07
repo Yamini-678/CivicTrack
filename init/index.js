b186518f403bcdf1b2d8eb6687495794af7768cf
Yamini-678 <yaminibisht678@gmail.com>
2025-05-07 19:11:39 +0530

    Initial project commit

---
+++ init/index.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const initData = require("./data.js");
+const Listing = require("../models/listing.js");
+
+const MONGO_URL = "mongodb://127.0.0.1:27017/Wanderra";
+
+main().then(()=>{
+    console.log("Connected to Database");
+}).catch((err)=>{
+    console.log(err);
+});
+
+async function main(){
+    await mongoose.connect(MONGO_URL);
+}
+
+const initDb = async() => {
+    await Listing.deleteMany({});
+    initDb.data = initData.data.map((obj) => ({
+        ...obj, owner:"681afbd6d0bb5906506ee63c"
+    }));
+    await Listing.insertMany(initData.data);
+    console.log("Data was intialized..");
+};
+
+initDb();
